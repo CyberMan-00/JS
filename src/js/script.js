ae520b41ddc4ad4e9dcf8b243b88cf16d099a188
Miran Shamaev <61889214+CyberMan-00@users.noreply.github.com>
2023-02-14 01:14:13 +0100

    program written (task 1,2,3)

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,19 @@
-'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
-const x = 'bob';
+const personalMovieDB = {
+	count:numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
-console.log(`https ://www.${x}.com`);
+for (let index = 0; index < numberOfFilms; index++) {
+	const lastMovie = prompt('Один из последних фильмов?','');
+	const lastMovieRating = prompt('На сколько его оцените?','');
+	personalMovieDB.movies[lastMovie] = lastMovieRating;
+};
 
+
+
+console.log(personalMovieDB);
