39f337016adfdeed7a55aa7b2bc90d8035ebcec6
Miran Shamaev <61889214+CyberMan-00@users.noreply.github.com>
2023-02-21 22:23:06 +0100

    test object created

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,52 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+'use strict';
+let numberOfFilms;
 
+
+function start() {
+	
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = prompt('Сколько фильмов вы уже посмотрели за последнее время?','');
+	}
+	return numberOfFilms;
+};
+// DB 
 const personalMovieDB = {
 	count:numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false
+	private: false
 };
 
-for (let index = 0; index < numberOfFilms; index++) {
-	const lastMovie = prompt('Один из последних фильмов?','');
-	const lastMovieRating = prompt('На сколько его оцените?','');
-	personalMovieDB.movies[lastMovie] = lastMovieRating;
-};
+function fillDB () {
+	let lastMovie;
+	let lastMovieRating;
+	for (let index = 0; index < numberOfFilms; index++) {
 
+	
+		lastMovie = prompt('Название одного из последних фильмов?','');
+		if (lastMovie == '' || lastMovie == null) {
+			index--;
+			continue;
+		}
+		lastMovieRating = +prompt('На сколько его оцените?','');
+		if (isNaN(lastMovieRating)) {
+			index--;
+			alert('rating must be a number');
+			continue;
+		}
+		personalMovieDB.movies[lastMovie] = lastMovieRating;
+	};
+	return personalMovieDB;
+}
 
+function showDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
 
-console.log(personalMovieDB);
+start();
+fillDB();
+showDB(personalMovieDB.private);
+
