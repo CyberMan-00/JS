d6f0ff1f69c641cdfa591221824ba533e05eea7b
Miran Shamaev <61889214+CyberMan-00@users.noreply.github.com>
2023-02-21 22:43:53 +0100

    destructuring object variables and functions

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,83 @@
 'use strict';
-let numberOfFilms;
+// let numberOfFilms;
 
 
-function start() {
+// function start() {
 	
-	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-		numberOfFilms = prompt('Сколько фильмов вы уже посмотрели за последнее время?','');
-	}
-	return numberOfFilms;
-};
-// DB 
-const personalMovieDB = {
-	count:numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	private: false
-};
+// 	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+// 		numberOfFilms = prompt('Сколько фильмов вы уже посмотрели за последнее время?','');
+// 	}
+// 	return numberOfFilms;
+// };
+// // DB 
+// const personalMovieDB = {
+// 	count:numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	private: false
+// };
 
-function fillDB () {
-	let lastMovie;
-	let lastMovieRating;
-	for (let index = 0; index < numberOfFilms; index++) {
+// function fillDB () {
+// 	let lastMovie;
+// 	let lastMovieRating;
+// 	for (let index = 0; index < numberOfFilms; index++) {
 
 	
-		lastMovie = prompt('Название одного из последних фильмов?','');
-		if (lastMovie == '' || lastMovie == null) {
-			index--;
-			continue;
-		}
-		lastMovieRating = +prompt('На сколько его оцените?','');
-		if (isNaN(lastMovieRating)) {
-			index--;
-			alert('rating must be a number');
-			continue;
-		}
-		personalMovieDB.movies[lastMovie] = lastMovieRating;
-	};
-	return personalMovieDB;
-}
+// 		lastMovie = prompt('Название одного из последних фильмов?','');
+// 		if (lastMovie == '' || lastMovie == null) {
+// 			index--;
+// 			continue;
+// 		}
+// 		lastMovieRating = +prompt('На сколько его оцените?','');
+// 		if (isNaN(lastMovieRating)) {
+// 			index--;
+// 			alert('rating must be a number');
+// 			continue;
+// 		}
+// 		personalMovieDB.movies[lastMovie] = lastMovieRating;
+// 	};
+// 	return personalMovieDB;
+// }
 
-function showDB(hidden) {
-	if (!hidden) {
-		console.log(personalMovieDB);
+// function showDB(hidden) {
+// 	if (!hidden) {
+// 		console.log(personalMovieDB);
+// 	}
+// }
+
+// start();
+// fillDB();
+// showDB(personalMovieDB.private);
+
+const student = {
+	name: 'miran',
+	surname: 'shamaev',
+	languages: {
+		python: 'intermediate',
+		javascript: 'beginner',
+		sql: 'intermediate'
+	},
+	learnNewLanguage: function(lang) {
+		console.log(`I learned ${lang}`);
+	},
+	functions: {
+		jump: function() {
+			console.log('I jumped');
+		},
+		run: function() {
+			console.log('I run');
+		}
 	}
-}
+};
 
-start();
-fillDB();
-showDB(personalMovieDB.private);
+console.log(student['languages']['python']);
+student.learnNewLanguage('java');
 
+// destructuring the object variables and functions
+const {python, sql} = student.languages;
+const {jump, run} = student.functions;
+
+console.log(python, sql);
+jump();
+run();
