df87f3ee691e5b4841b7a71c8788b421661776d8
Miran Shamaev <61889214+CyberMan-00@users.noreply.github.com>
2023-11-03 00:44:52 +0100

    cleaned branch

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,39 @@
 'use strict';
-console.log('hello');
+
+const plotQueue = ['block', 'block'];
+
+
+class BlockEvent {
+	constructor(messageToPlayer, messageFromPlayer, triggerWord, blockTimer) {
+		this.messageToPlayer = messageToPlayer;
+		this.messageFromPlayer = messageFromPlayer;
+		this.triggerWord = triggerWord;
+		this.blockTimer = blockTimer;
+
+	}
+
+	triggerEvent() {
+		if (this.messageFromPlayer.contains(this.triggerWord)) {
+			return 'event is triggered';
+		}
+	}
+
+};
+
+
+
+// eventOne() {
+// 	console.log('eventOne');
+// };
+// eventTwo() {
+// 	console.log('eventOne');
+// };
+// eventThree() {
+// 	console.log('eventOne');
+// };
+
+// BlockEvent.eventList = [BlockEvent.eventOne, BlockEvent.eventTwo, BlockEvent.eventThree];
+
+const instance = new BlockEvent('hello', 'bye bye!', '!', '10sec');
+
+console.log(instance.triggerEvent());
